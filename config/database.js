9ae3f98e3e60a124e72f2a507678a9991a52a6e9
butterma <butterma@g.jct.ac.il>
2018-06-26 20:20:02 +0300

    fix

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports={
+    'url':'mongodb://user:user@yochidafna-shard-00-00-zemea.mongodb.net:27017,yochidafna-shard-00-01-zemea.mongodb.net:27017,yochidafna-shard-00-02-zemea.mongodb.net:27017/shecodes?ssl=true&replicaSet=YochiDafna-shard-0&authSource=admin'
+}
